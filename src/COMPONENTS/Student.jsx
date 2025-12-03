aedbd68a88c1e3a11ede1bf23110a3d5376ee791
jiyanshuj <jainjiyanshu@gmail.com>
2025-12-03 13:32:57 +0530

    update backend URL

--- src/COMPONENTS/Student.jsx
+++ src/COMPONENTS/Student.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Camera, CheckCircle, XCircle, AlertCircle, Zap, Play } from 'lucide-react';
 
-const API_BASE = import.meta?.env?.VITE_API_BASE || 'http://127.0.0.1:8000';
+const API_BASE ='https://e842b2d53b06.ngrok-free.app';
 
 function Student() {
     const [page, setPage] = useState('home');
